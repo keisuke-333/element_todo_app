f8742c7c9df1d8701fc98a2e8157b3d355d916fb
keisuke-333 <keikubo.999@gmail.com>
2020-08-01 00:55:22 +0900

    updateアクションを追加

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -20,6 +20,15 @@ class ToDosController < ApplicationController
     @to_do = ToDo.find_by(id: params[:id])
   end
 
+  def update
+    @to_do = ToDo.find_by(id: params[:id])
+    if @to_do.update(to_do_params)
+      redirect_to to_dos_url
+    else
+      render :edit
+    end
+  end
+
   private
 
   def to_do_params
