27d6fe96f6ce19c7ef57a47620c7a5441aa9f6b4
keisuke-333 <keikubo.999@gmail.com>
2020-08-03 17:29:03 +0900

    vue.jsでtodo一覧のJSONを取得

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -11,29 +11,13 @@ import axios from "axios";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-02",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-04",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2016-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-      ],
+      toDos: [],
     };
+  },
+  created() {
+    axios.get("/api/v1/to_dos").then((res) => {
+      this.toDos = res.data;
+    });
   },
 };
 </script>
