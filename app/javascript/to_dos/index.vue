882dd3cfad44bdfd8353931ecb06f1ef173c9817
keisuke-333 <keikubo.999@gmail.com>
2020-08-03 23:21:36 +0900

    削除機能を実装

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -3,13 +3,16 @@
     <el-table-column prop="title"></el-table-column>
     <el-table-column prop="expired_at"></el-table-column>
     <el-table-column width="120">
-      <el-button @click="destroyToDo" type="danger" icon="el-icon-delete" circle></el-button>
+      <template v-slot="scope">
+        <el-button @click="destroyToDo(scope.row.id)" type="danger" icon="el-icon-delete" circle></el-button>
+      </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
 import axios from "axios";
+import { reject } from "lodash";
 export default {
   data() {
     return {
@@ -20,6 +23,15 @@ export default {
     axios.get("/api/v1/to_dos").then((res) => {
       this.toDos = res.data;
     });
+  },
+  methods: {
+    destroyToDo(id) {
+      axios.delete("/api/v1/to_dos/" + id).then((res) => {
+        if (res.status === 200) {
+          this.toDos = reject(this.toDos, ["id", id]);
+        }
+      });
+    },
   },
 };
 </script>
