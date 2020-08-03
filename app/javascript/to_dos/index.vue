7a883ae74bc19d57eda8df9a0f0a9f8ab3a01f9b
keisuke-333 <keikubo.999@gmail.com>
2020-08-03 18:10:42 +0900

    取得したToDoの一覧をElement UIのテーブルに表示

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,8 +1,7 @@
 <template>
-  <el-table :data="tableData" style="width:100%">
-    <el-table-column prop="date" label="Date" with="180"></el-table-column>
-    <el-table-column prop="name" label="Name" width="180"></el-table-column>
-    <el-table-column prop="address" label="Address"></el-table-column>
+  <el-table :data="toDos" style="width:100%">
+    <el-table-column prop="title"></el-table-column>
+    <el-table-column prop="expired_at"></el-table-column>
   </el-table>
 </template>
 
