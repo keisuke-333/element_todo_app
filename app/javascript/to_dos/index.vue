ac91bb284236c239404aca01e10773f2db018e4f
keisuke-333 <keikubo.999@gmail.com>
2020-08-05 17:46:48 +0900

    ボタンでダイアログ表示を切り替え

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <el-row>
+      <el-button icon="el-icon-plus" @click="createToDoDialog = true" circle></el-button>
       <el-col :span="12" :offset="6">
         <el-tabs v-model="activeName">
           <el-tab-pane label="ToDo" name="toDo">
@@ -20,12 +21,15 @@
         </el-tabs>
       </el-col>
     </el-row>
+    <el-dialog :visible.sync="createToDoDialog" width="30%" center>
+      <to-do-form></to-do-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import ToDoForm from "../to_dos/to-do-form";
 import ToDoTable from "../to_dos/to-do-table";
+import ToDoForm from "../to_dos/to-do-form";
 import axios from "axios";
 import { reject, filter } from "lodash";
 export default {
@@ -33,11 +37,12 @@ export default {
     return {
       toDos: [],
       activeName: "toDo",
+      createToDoDialog: false,
     };
   },
   components: {
     ToDoTable,
-    ToDoTable,
+    ToDoForm,
   },
   created() {
     axios.get("/api/v1/to_dos").then((res) => {
